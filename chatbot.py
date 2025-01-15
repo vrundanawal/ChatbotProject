ee520dffe39828cc4bdd37aba5fab80177ca7a75
vrundanawal <nawal.vrunda@gmail.com>
2025-01-15 12:11:52 +0000

    Extract the text from PDF

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from PyPDF2 import PdfReader
 
 #Upload PDF file
 st.header("My First Chatbot")
@@ -6,3 +7,12 @@ st.header("My First Chatbot")
 with st.sidebar:
     st.title("Your Documents")
     file = st.file_uploader("Upload a PDF file and start asking questions", type="pdf")
+
+#Extract the text
+if file is not None:
+    pdf_reader = PdfReader(file)
+    text = ""
+    for page in pdf_reader.pages:
+        text += page.extract_text()
+        st.write(text)
+#st.write(text)
